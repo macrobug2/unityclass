323cbb8768394818a0bb910cb2a64091153ca142
Pearson Lawrence <s179130@students.aie.edu.au>
2018-06-06 21:06:40 -0700

    Added and manipulated Unit Rigidbody

---
+++ Assets/_Scripts/BallController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    private Rigidbody RB;
+
+    public float Speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        RB = GetComponent<Rigidbody>();
+	}
+	// Update is called once per frame
+	void Update ()
+    {
+        float LeftRight = Input.GetAxisRaw("Horizontal");
+        float ForwardBack = Input.GetAxisRaw("Vertical");
+
+        Vector3 Dir = new Vector3(LeftRight,0,ForwardBack);
+
+
+        RB.AddForce(Dir * Time.deltaTime * Speed);
+
+        
+	}
+
+
+}
